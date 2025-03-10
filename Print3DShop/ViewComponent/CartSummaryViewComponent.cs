ae01dca9f34757e7d0354fb393f98255e5d1509d
gelmius <selmius@gmail.com>
2025-03-10 21:23:08 +0200

    added views

---
+++ Print3DShop/ViewComponent/CartSummaryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Print3DShop.Services;
+
+namespace Print3DShop.ViewComponents
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private readonly IShoppingCartService _shoppingCartService;
+
+        public CartSummaryViewComponent(IShoppingCartService shoppingCartService)
+        {
+            _shoppingCartService = shoppingCartService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var count = await _shoppingCartService.GetCountAsync();
+            return View(count);
+        }
+    }
+}
